302214aabcaf9aa81f6889c3fc3e5daf2a41dd9f
Renato Rocha de Almeida <rocha@Renatos-MBP.hub>
2021-12-10 12:42:04 +1300

    refactor: remove instagram link

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -1,13 +1,11 @@
 import { Behance } from "@styled-icons/boxicons-logos/Behance"
 import { Github } from "@styled-icons/boxicons-logos/Github"
 import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
-import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
 
 const Icons = {
   Behance,
   Github,
   Linkedin,
-  Instagram
 }
 
 export default Icons
--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -1,19 +1,15 @@
 const links = [
   {
     label: "Github",
-    url: "https://github.com/renatorocha1/"
+    url: "https://github.com/renatorocha1/",
   },
   {
     label: "Linkedin",
-    url: "https://www.linkedin.com/in/renatorocha1"
+    url: "https://www.linkedin.com/in/renatorocha1",
   },
   {
     label: "Behance",
-    url: "https://www.behance.net/renatorocha3"
-  },
-  {
-    label: "Instagram",
-    url: "https://www.instagram.com/renatorocha.dev/"
+    url: "https://www.behance.net/renatorocha3",
   },
 ]
 
