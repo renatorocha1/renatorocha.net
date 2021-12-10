14cc3d024c6e5fbe94aff83d1cba3574dc1287eb
Renato Rocha de Almeida <rocha@Renatos-MBP.hub>
2021-12-10 13:51:28 +1300

    feat: add a contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import Layout from "./../components/Layout"
 import SEO from "./../components/seo"
 
 import * as S from "./../components/Post/styled"
+import Button from "./../components/Button"
 
 const ContactPage = () => (
   <Layout>
@@ -11,9 +12,23 @@ const ContactPage = () => (
       <S.PostTitle>Contact</S.PostTitle>
     </S.PostHeader>
     <S.MainContent>
-      <p>
-        Coming soon...
-      </p>
+      <article>
+        <p>Let's make something together.</p>
+        <p>
+          Feel free to send me an email if you're looking for a software
+          developer, have a question or just want to say hi! 👋🏽
+        </p>
+        <p>
+          <Button
+            href="mailto:hi@renatorocha.net"
+            title="My email to contact"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Say hello
+          </Button>
+        </p>
+      </article>
     </S.MainContent>
   </Layout>
 )
